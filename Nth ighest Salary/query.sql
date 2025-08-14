baeb1f3180ab9a807656b6fcdaf9c24fc8c57397
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-08-14 23:38:09 +0530

    Update query.sql

--- Nth ighest Salary/query.sql
+++ Nth ighest Salary/query.sql
@@ -1,8 +1,16 @@
+delimiter //
+
 create function getNthHighestSalary(n int) returns int
+deterministic
 begin
-  return (
+  declare result int;
+
   select distinct salary
+  into result
   from employee
   order by salary desc
   limit 1 offset n-1
-  );
+
+  return result;
+end//
+delimiter;
