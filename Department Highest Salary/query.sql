567ffd21e8b5d8c5656d370c3a2d0261b35f7a86
Shashwat Dwivedi <67761122+coolrishi28@users.noreply.github.com>
2025-08-15 21:14:39 +0530

    Update query.sql

--- Department Highest Salary/query.sql
+++ Department Highest Salary/query.sql
@@ -1,11 +1,11 @@
 -- Table is named as employees and has columns named as salary, name, departmentid
--- Table is named as departmet and has columns named as id, name
+-- Table is named as department and has columns named as id, name
 
 select d.name as department, e.name as employee, e.salary
 from employees e
 join department d on e.departmentid = d.id
 where (e.salary, e.departmentid) in (
   select max(salary), departmentid
-  from employee
+  from employees
   group by departmentid
 );
